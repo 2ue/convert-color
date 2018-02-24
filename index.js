f3b1cc101c5c2e54fccea00db8cd54fc14bc55b9
2ue <jie7466358356@153.com>
2018-02-24 21:50:33 +0800

    优化

--- index.js
+++ index.js
@@ -2,17 +2,18 @@
  * Created by 2ue on 2017/8/20.
  */
 
-function color(short) {
-    this.short = typeof short === 'undefined' || short ? true : false;
+function color(config) {
+    this.short = typeof config.short === 'undefined' || config.short ? true : false;
     this.rgbReg = /^rgb\(([0-9]{1,3}\,){2}([0-9]{1,3})\)\;?$/;
     this.hexReg = /^#([0-9a-f]{3}|[0-9a-f]{6})$/;
 }
 
 color.prototype = {
     constructor: color,
-    /*
-    * @function 16进制(Hexadecimal)色值转换成RGB色值
-    * @return 返回形如rgb(0,0,0)的RGB色值
+    /**
+     * 16进制(Hexadecimal)色值转换成RGB色值
+     * @param {String} color 颜色值
+     * @return 返回形如rgb(0,0,0)的RGB色值
     */
     toRgb: function (color) {
         var match = color.match(this.hexReg), matchVal, len, dis;
@@ -27,13 +28,14 @@ color.prototype = {
             }
             return res.join('');
         }else {
-            return this;
+            return null;
         }
     },
-    /*
-    * @function RGB色值转换成16进制(Hexadecimal)色值
-    * @params {Boolean} 是否简写，默认为true
-    * @return 返回形如#ff0000或#f00的16进制色值
+    /**
+     * RGB色值转换成16进制(Hexadecimal)色值
+     * @param {String} color 颜色值
+     * @param {Boolean} 是否简写，默认为true
+     * @return 返回形如#ff0000或#f00的16进制色值
     */
     toHex: function (color, short) {
         color = color.replace(/\s/g, '');
@@ -41,7 +43,7 @@ color.prototype = {
         //首先验证是否符合格式，允许空格存在
         //形如rgb(0,255,0)或者rgb(0,255,0);
         if(match){
-            var res = ['#','','',''], matchVal = color.match(/\d+/g), len = matchVal.length, isShort = typeof short === 'undefined' ? this.short : !!short;
+            var res = ['#', '', '', ''], matchVal = color.match(/\d+/g), len = matchVal.length, isShort = typeof short === 'undefined' ? this.short : !!short;
             for(var i = 0; i < len; i++){
                 var val = matchVal[i], xVal = Number(val).toString(16), single = val < 16, sameChar = single || val % 17 === 0;
                 if(!sameChar) isShort = false;
@@ -50,7 +52,7 @@ color.prototype = {
             res = res.join('').split('');
             return isShort ? [res[0], res[1], res[3], res[5]].join('') : res.join('');
         }else {
-            return this;
+            return null;
         }
     }
 };
